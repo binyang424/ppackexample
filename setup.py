a49ce908758c0585a54366034b8a701ff5b8473c
Bin <bin.yang@polymtl.ca>
2022-04-30 23:54:58 -0400

    text update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ppackexample',
-    version='0.1.0',    
+    version='0.0.1',    
     description='A example Python package',
     url='https://github.com/shuds13/ppackexample',
     author='Bin Yang',
---
+++ test.py
@@ -0,0 +1,5 @@
+from ppackexample import add
+
+c = add.add(1,2)
+
+print(c)
